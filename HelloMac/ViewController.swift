5ea964df26fbde1deb2db69e6794756ffa40c460
abuzeid <abozaid.hamed@vodafone.com>
2018-12-25 10:08:48 +0200

    Initial Commit

---
+++ HelloMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloMac
+//
+//  Created by Abuzeid Ibrahim on 12/25/18.
+//  Copyright © 2018 abuzeid. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
